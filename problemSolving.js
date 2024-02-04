4c538890150d77bbf0a8a0cb59b1e61f7fb70583
abdur-rhaman-babu <abdulaziz139953@gmail.com>
2024-02-04 21:11:59 +0600

    solve

--- problemSolving.js
+++ problemSolving.js
@@ -16,16 +16,16 @@ const babuLength = matches ? matches.length : 0
 
 
 // problem no 2
-const arr = ['a','b','c','d', 'c']
+const findString1 = ['a','b','c','d', 'c']
 const leanearSearch = (arr,c)=>{
     const firstPositionOfc = arr.indexOf(c)
     return firstPositionOfc >=0 ? firstPositionOfc : 'not found';
 }
 
-// console.log(leanearSearch(arr,'c'))
+// console.log(leanearSearch(findString1,'c'))
 
 
-const arr1 = ['a','b','c','d', 'c']
+const findString = ['a','b','c','d', 'c']
 const leanearSearch1 = (arr,c)=>{
     for(let i=0; i<arr.length; i++){
         if(arr[i] === c){
@@ -35,7 +35,7 @@ const leanearSearch1 = (arr,c)=>{
     return 'not found'
 }
 
-// console.log(leanearSearch(arr,'c'))
+// console.log(leanearSearch(findString,'c'))
 
 // find largest string
 
@@ -51,3 +51,36 @@ const getLargestString  = (arr) =>{
 }
 
 console.log(getLargestString(arrString))
+
+const arr = [25,36,100,7,8,22,1,55,99,88,102,12,105]
+
+// array sum
+let sum = 0;
+for(let i=0; i<arr.length; i++){
+    sum+=arr[i]
+}
+// console.log(sum)
+
+// find largest number 
+let largestNumber = arr[0];
+for(let i=1; i<arr.length; i++){
+    if(arr[i]>largestNumber){
+        largestNumber = arr[i]
+        // console.log(largestNumber)
+    }
+}
+console.log(`largest numer is ${largestNumber}`)
+
+// find smallest number 
+let smallestNumber = arr[0];
+for(let i=1; i<arr.length; i++){
+    if(arr[i]<smallestNumber){
+        smallestNumber = arr[i]
+        // console.log(smallestNumber)
+    }
+}
+console.log(`smallest number is ${smallestNumber}`)
+
+
+
+  
